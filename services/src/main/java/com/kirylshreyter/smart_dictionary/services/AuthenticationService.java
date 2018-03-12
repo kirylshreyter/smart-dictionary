d5f20f91324ad99325773926fcc16bf0d96da9a1
Kiryl Shreyter <k.shreyter@unlitsolutions.com>
2018-03-12 12:00:03 +0300

    authentication service.

---
+++ services/src/main/java/com/kirylshreyter/smart_dictionary/services/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.kirylshreyter.smart_dictionary.services;
+
+import com.kirylshreyter.smart_dictionary.services.security.Authentication;
+import com.kirylshreyter.smart_dictionary.services.security.Credentials;
+
+public interface AuthenticationService {
+
+	public Authentication authenticate(Credentials credentials);
+
+	public Authentication authenticate(String token);
+
+	public Credentials resolveCredentials(String base64encodedString);
+
+	public boolean nullifyAuthentication(String token);
+
+	public String generateToken();
+
+}
