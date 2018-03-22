e78344818336f235d2b40e5e5dfe350195ea1418
Kiryl Shreyter <k.shreyter@unlitsolutions.com>
2018-03-22 10:37:47 +0300

    delete unnecessary models.

--- dao-db/src/main/java/com/kirylshreyter/smart_dictionary/daodb/FirstNameDaoDb.java
+++
@@ -1,7 +0,0 @@
-package com.kirylshreyter.smart_dictionary.daodb;
-
-import com.kirylshreyter.smart_dictionary.datamodel.IFirstName;
-
-public interface FirstNameDaoDb extends AbstractEntityDaoDb<IFirstName> {
-
-}
--- webapp/src/main/java/com/kirylshreyter/smart_dictionary/webapp/models/FirstNameModel.java
+++
@@ -1,9 +0,0 @@
-package com.kirylshreyter.smart_dictionary.webapp.models;
-
-public interface FirstNameModel extends AbstractModel {
-
-	public String getValue();
-
-	public void setValue(String value);
-
-}
