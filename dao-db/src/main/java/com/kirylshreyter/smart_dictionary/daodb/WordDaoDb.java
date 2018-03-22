3b57a8cedf559fab324f953bde55433db5fce8d8
Kiryl <kirylshreyter@gmail.com>
2018-03-22 11:36:16 +0300

    Merge pull request #62 from kirylshreyter/development
    Development

--- dao-db/src/main/java/com/kirylshreyter/smart_dictionary/daodb/WordDaoDb.java
+++ dao-db/src/main/java/com/kirylshreyter/smart_dictionary/daodb/WordDaoDb.java
@@ -1,7 +1,15 @@
 package com.kirylshreyter.smart_dictionary.daodb;
 
+import java.util.List;
+
 import com.kirylshreyter.smart_dictionary.datamodel.IWord;
 
 public interface WordDaoDb extends AbstractEntityDaoDb<IWord> {
+
+    public IWord findByIdAndUserId(long id, long userId);
+
+    public List<IWord> findAllByUserId(long userId);
+
+    public void deleteOrphanTranslations();
 
 }
--- dao-db/src/main/java/com/kirylshreyter/smart_dictionary/daodb/impl/WordDaoDbImpl.java
+++ dao-db/src/main/java/com/kirylshreyter/smart_dictionary/daodb/impl/WordDaoDbImpl.java
@@ -1,11 +1,38 @@
 package com.kirylshreyter.smart_dictionary.daodb.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kirylshreyter.smart_dictionary.daodb.WordDaoDb;
+import com.kirylshreyter.smart_dictionary.daodb.repositories.TranslationRepository;
+import com.kirylshreyter.smart_dictionary.daodb.repositories.WordRepository;
 import com.kirylshreyter.smart_dictionary.datamodel.IWord;
 
 @Repository
 public class WordDaoDbImpl extends AbstractEntityDaoDbImpl<IWord> implements WordDaoDb {
+
+    @Autowired
+    private WordRepository wordRepository;
+
+    @Autowired
+    private TranslationRepository translationRepository;
+
+    @Override
+    public IWord findByIdAndUserId(long id, long userId) {
+	return wordRepository.findByIdAndUserId(id, userId);
+    }
+
+    @Override
+    public List<IWord> findAllByUserId(long userId) {
+	return wordRepository.findAllByUserId(userId).stream().map(s -> s).collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteOrphanTranslations() {
+	translationRepository.deleteOrphanTranslations();
+    }
 
 }
